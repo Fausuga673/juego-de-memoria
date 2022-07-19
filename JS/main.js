806f3f4b0b0e39fa8cbdeb4471e53aeb7849f583
Frey Usuga <fausuga673@misena.edu.co>
2022-07-19 16:57:44 -0500

    Las imagenes ahora toman un id

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,19 @@
 const albumContainer = document.getElementById('album-container');
-const albumItem = document.querySelector('.album')
 
-for (a = 1; a <= 2 ; a++) {
+let id = 1;
 
-    let album = [1, 2, 3, 4, 5, 6, 7, 8];
-    album = album.sort(function() {return Math.random() - 0.5});
+for (i = 1; i <= 2 ; i++) {
 
-    for (let position = [0, 1, 2, 3, 4, 5, 6, 7], i = position.length; i--; ) {
+    let albumRandomValue = [1, 2, 3, 4, 5, 6, 7, 8];
+    albumRandomValue = albumRandomValue.sort(function() {return Math.random() - 0.5});
+    
+    for (let albumPosition = [0, 1, 2, 3, 4, 5, 6, 7], i = albumPosition.length; i--; ) {
         
-        let valueRandom = position.splice(Math.trunc(Math.random() * (i + 1)), 1)[0];
-        console.log(valueRandom);
-
-        albumContainer.innerHTML += `<button class="album" value="${album[valueRandom]}"></div><img class="album__img" src="img/album${album[valueRandom]}.jpg"></button>`;
-
+        let valuePositionRandom = albumPosition.splice(Math.trunc(Math.random() * (i + 1)), 1)[0];
+        console.log('album random: ' + albumRandomValue[valuePositionRandom]);
+        
+        albumContainer.innerHTML += `<img class='album__img' id='${id++}' src='IMG/album${albumRandomValue[valuePositionRandom]}.jpg'>`;
+        
     }
-
+    
 }
